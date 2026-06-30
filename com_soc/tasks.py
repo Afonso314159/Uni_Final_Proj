acefb85abdd915806f8f8a08755e08c537f80e65
Afonso Pereira <148291218+Afonso314159@users.noreply.github.com>
2026-06-30 19:29:20 +0100

    Update tasks.py

--- com_soc/tasks.py
+++ com_soc/tasks.py
@@ -1,8 +1,10 @@
 from celery import shared_task
 from datetime import date, timedelta
-from .models import Subscricao
+from django.utils import timezone
+from django.shortcuts import get_object_or_404
+from .models import Subscricao, Noticia, ModerationConfig
 from .stripe_emails import send_renewal_warning_email
-from .utils import create_notification
+from .utils import create_notification, AI_score
 
 @shared_task
 def enviar_avisos_renovacao():
@@ -25,3 +27,35 @@ def enviar_avisos_renovacao():
         except Exception as e:
             print(f'Erro ao notificar {user.username}: {e}')
     return f'{count} aviso(s) enviado(s).'
+
+@shared_task
+def evaluate_noticia_task(noticia_id):
+    try:
+        noticia_obj = Noticia.objects.get(id=noticia_id)
+    except Noticia.DoesNotExist:
+        print(f"evaluate_noticia_task: Noticia {noticia_id} não encontrada.")
+        return
+
+    config = get_object_or_404(ModerationConfig, name="default")
+    evaluation = AI_score(noticia_obj, config)
+    noticia_obj.ai_evaluation = evaluation
+    risk_level = evaluation.get('risk_level', 'medium')
+
+    if risk_level == 'trash':
+        message = f"A notícia '{noticia_obj.titulo}' foi rejeitada."
+        create_notification(noticia_obj.autor, message)
+        noticia_obj.delete()
+        return
+
+    elif risk_level == 'ideal':
+        noticia_obj.estado_publicacao = Noticia.EstadoPublicacao.PUBLICADA
+        noticia_obj.data_publicacao = timezone.now().date()
+        message = f"A notícia '{noticia_obj.titulo}' foi aceite e publicada."
+        create_notification(noticia_obj.autor, message)
+
+    else:
+        noticia_obj.estado_publicacao = Noticia.EstadoPublicacao.PENDENTE
+        message = f"A notícia '{noticia_obj.titulo}' esta pendente a espera de revisao."
+        create_notification(noticia_obj.autor, message)
+
+    noticia_obj.save()
